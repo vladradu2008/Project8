e5f55941ce5642495a13f586951cfb3191078913
vladradu2008 <vlad.radu2013@yahoo.com>
2013-11-03 18:00:06 +0200

    am facut functia lalal

--- src/project8/Project8.java
+++ src/project8/Project8.java
@@ -1,19 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package project8;
 
-/**
- *
- * @author Ilie,  Radu & Mandrescu
- */
+import java.awt.*;
+import javax.swing.*;
+
+
+
+
+
+
+
 public class Project8 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
 }
