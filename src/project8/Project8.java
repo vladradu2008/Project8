d6bfd22df85cc47566a3d12a757a05f689ba433e
Stefan Ilie <ilie.stefan92@yahoo.com>
2013-11-03 17:51:54 +0200

    First Commit

---
+++ src/project8/Project8.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project8;
+
+/**
+ *
+ * @author Ilie,  Radu & Mandrescu
+ */
+public class Project8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
